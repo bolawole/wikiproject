4334714227e72a3cf6d16adedc135bc2bc063bc4
bolawole <dewoleawe@gmail.com>
2020-07-20 20:16:04 -0400

    added search feature

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,41 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from . import util
+import markdown
 
+md=markdown.Markdown()
 
 def index(request):
+    if request.method=="POST":
+        data=request.POST.get('search_value').lower()
+        entry_list=[x.lower() for x in util.list_entries()]
+        if data in entry_list:
+            html_body=util.get_entry(request.POST.get('search_value'))
+            return render(request, "encyclopedia/entries.html", {"content":md.convert(html_body)})
+        elif matches(data):
+            print(f"\n\n{matches(data)}\n\n")
+            return HttpResponse("i will get back to you")
+        else:
+            return HttpResponse("Nothing")
+
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request,entry):
+    
+    html_body=util.get_entry(entry)
+    print(f"\n\n {html_body}\n\n")
+    return render(request, "encyclopedia/entries.html", {"content":md.convert(html_body)})
+   
+    
+
+def matches(data):
+    match=[]
+    entry_list=[x.lower() for x in util.list_entries()]
+    for each_list in entry_list:
+        if data in each_list:
+            match.append(each_list)
+    return match
+    
