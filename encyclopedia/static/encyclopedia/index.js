8d2641b7290e82191a8b76d2dbe37a76bb86f780
bolawole <dewoleawe@gmail.com>
2020-07-25 10:36:22 -0400

    ..

---
+++ encyclopedia/static/encyclopedia/index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+//    var entry_h1=document.querySelector(".entry_display").querySelectorAll("h1");
+//    var entry_p=document.querySelector(".entry_display").childNodes[3];
+//    entry_p.setAttribute("id","entry_p")
+//    entry_h1.forEach(h1=>{
+//     h1.setAttribute("class", "entry_header");
+//    })
+   
+   
+//    console.log(document.querySelector(".entry_display"));
+
+});
